e44a6893b1c981967fc6cd4372359407aecad139
Slipsoon <sebastian.slipek@hotmail.com>
2020-04-27 16:57:26 +0200

    Loop ex.

---
+++ SumOddRange/src/SumOddRange.java
@@ -0,0 +1,26 @@
+public class SumOddRange {
+
+    public static boolean isOdd(int number) {
+
+        if (number > 0){
+            return number % 2 == 1;
+        }
+
+        return false;
+    }
+
+    public static int sumOdd(int start, int end) {
+
+        if ((end < start) || (start < 0) || (end < 0))
+            return -1;
+
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+
+            sum = (isOdd(i)) ? sum += i : sum;
+        }
+
+        return sum;
+    }
+}
